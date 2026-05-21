33c5e598d61566067ca52fb6c6d0c5b197e98982
Yael <243678@ids.upchiapas.edu.mx>
2026-05-21 02:05:07 -0600

    [refactor] Reubicación de escena

--- entities/props/scripts/crate.gd
+++ entities/props/scripts/crate.gd
@@ -2,5 +2,11 @@ extends RigidBody3D
 
 @onready var hit_box_component: HitBoxComponent = %HitBoxComponent
 
+
 func damage(attack: Attack):
 	hit_box_component.damage(attack)
+
+
+func _on_health_component_has_died() -> void:
+	print("destruido")
+	queue_free()
