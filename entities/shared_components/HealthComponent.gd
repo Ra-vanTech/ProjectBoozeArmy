beccbfd581b5ebd5599067ac49ec7f4b604c5129
Yael <243678@ids.upchiapas.edu.mx>
2026-05-21 02:14:29 -0600

    [feat] Mejoras de componente de salud
    Se añade señal de muerte para un mejor manejo del cambio de estado
    Se separa la vida inicial de la actual
    se añade la cantidad de monedas soltadas al morir
    Se añaden las monedas al banco al morir

--- entities/shared_components/HealthComponent.gd
+++ entities/shared_components/HealthComponent.gd
@@ -1,15 +1,26 @@
-class_name HealthComponent extends Node
+class_name HealthComponent
+extends Node
 
+signal has_died
 
-@export var MAX_HEALTH: float = 100.0
-var health:float
+@export var STARTING_HEALTH: float = 100.0
+@export var COINS_DROPPED_DEFAULT: int = 25
+
+var health: float
+var coins_dropped: int
+
+@onready var bank_label: Bank = get_tree().get_first_node_in_group("bank")
+
 
 func _ready() -> void:
-	health = MAX_HEALTH
+	health = STARTING_HEALTH
+	coins_dropped = COINS_DROPPED_DEFAULT
+
 
 func damage(attack: Attack):
 	health -= attack.damage
 	print(health)
 
 	if health <= 0:
-		get_parent().queue_free()
+		bank_label.gold += coins_dropped
+		has_died.emit()
