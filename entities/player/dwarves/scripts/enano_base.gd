dae0f3174f3b24a6ab2fdd0de6a79c0c941fb024
MishellPG <pradogordillomichael@gmail.com>
2026-05-28 13:06:48 -0600

    fix: definimos la base matematica de daño, prepare ejecutar ataque en cuanto elabore las FSM

--- entities/player/dwarves/scripts/enano_base.gd
+++ entities/player/dwarves/scripts/enano_base.gd
@@ -1,70 +1,53 @@
 class_name EnanoBase
-extends Node3D
+extends CharacterBody3D
+
+@onready var attack_range: Area3D = %AttackRange
+@onready var state_machine: StateMachine = %StateMachine
 
 @export var damage: float = 10.0
-@export var attack_cooldown_time: float = 1.2
+@export var cooldown_base: float = 1.0
 
-@onready var attack_range: Area3D = $AttackRange
-@onready var attack_cooldown: Timer = $AttackCooldown
-
+#estado dinamico 
 var enemies_in_range: Array[Node3D] = []
-var can_attack: bool = false
 
 func _ready() -> void:
-    attack_cooldown.wait_time = attack_cooldown_time
-    attack_range.body_entered.connect(_on_body_entered)
-    attack_range.body_exited.connect(_on_body_exited)
-    attack_range.area_entered.connect(_on_area_entered)
-    attack_range.area_exited.connect(_on_area_exited)
-    attack_cooldown.timeout.connect(_on_attack_cooldown_timeout)
-    
-    # Desincronización de timers iniciales
-    var initial_delay: float = randf_range(0.0, attack_cooldown_time)
-    attack_cooldown.start(initial_delay)
+  attack_range.body_entered.connect(_on_attack_range_body_entered)
+  attack_range.body_exited.connect(_on_attack_range_body_exited)
 
-func _process(_delta: float) -> void:
-    if not can_attack:
-        return
-        
-    #FIX: Limpiar nodos destruidos primero
-    limpiar_enemigos_muertos()
-    
-    if enemies_in_range.size() > 0:
-        _attack(enemies_in_range[0])
 
-#FIX: Función auxiliar para evitar crashes
-func limpiar_enemigos_muertos() -> void:
-    for i in range(enemies_in_range.size() - 1, -1, -1):
-        if not is_instance_valid(enemies_in_range[i]):
-            enemies_in_range.remove_at(i)
+func _physics_process(delta: float) -> void:
+    #comportamiento de FSM
+  state_machine.tick(delta)
 
+
+# Metodos de calculo de daño 
+func obtener_modificador_ebriedad() -> float:
+    return 1.0 # simula el nivel 1
+
+func obtener_modificador_upgrades() -> float:
+    return 0.0 # simula el nivel 1
+
+func obtener_daño_final() -> float:
+    var mod_ebriedad: float = obtener_modificador_ebriedad()
+    var mod_upgrades: float = obtener_modificador_upgrades()
+
+    #retorna la formula de daño final 
+    return damage * mod_ebriedad * (1.0 + mod_upgrades)
+
+
+# Acciones base (poliformismo) 
 func _attack(target: Node3D) -> void:
-    if target.has_method("damage"):
-        var atk := Attack.new()
-        atk.damage = damage
-        target.damage(atk)
-        can_attack = false
-        attack_cooldown.start()
-        print("[DEBUG] Enano '%s' ataca en frame %d" % [name, Engine.get_process_frames()])
+   if not is_instance_valid(target):
+    return
+   var attack := Attack.new()
+   attack.damage = obtener_daño_final()
+   target.damage(attack)
 
-func _on_body_entered(body: Node3D) -> void:
-    if body is EnemyBase:
-        if not enemies_in_range.has(body):
-            enemies_in_range.append(body)
+# señales de deteccion 
+func _on_attack_range_body_entered(body: Node3D) -> void:
+    if body.is_in_group("enemy") and not enemies_in_range.has(body):
+      enemies_in_range.append(body)
 
-func _on_body_exited(body: Node3D) -> void:
-    enemies_in_range.erase(body)
-
-func _on_area_entered(area: Area3D) -> void:
-    var parent := area.get_parent()
-    if parent is EnemyBase:
-        if not enemies_in_range.has(parent):
-            enemies_in_range.append(parent)
-
-func _on_area_exited(area: Area3D) -> void:
-    var parent := area.get_parent()
-    if parent is EnemyBase:
-        enemies_in_range.erase(parent)
-
-func _on_attack_cooldown_timeout() -> void:
-    can_attack = true
+func _on_attack_range_body_exited(body: Node3D) -> void:
+    if body.is_in_group("enemy") and enemies_in_range.has(body):
+        enemies_in_range.erase(body)
