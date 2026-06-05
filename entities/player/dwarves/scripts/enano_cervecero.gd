b10764685484523d2e7a9495d2eb8d0e4bc85a26
MishellPG <pradogordillomichael@gmail.com>
2026-06-04 21:06:05 -0600

    fix: esta funcion ya esta para el scrip de enanoBase, se encarga de esa responsabilidad

--- entities/player/dwarves/scripts/enano_cervecero.gd
+++ entities/player/dwarves/scripts/enano_cervecero.gd
@@ -12,10 +12,3 @@ func _ready() -> void:
 # MOCKUP para simular el estado de ebriedad en el enano
 func _generar_ebriedad() -> void:
 	print("[EnanoCervecero] Genera %.1f ebriedad (MOCKUP)" % EBRIEDAD_SEGUNDO)
-
-
-# Sobrescribe el modificador de ebriedad de EnanoBase
-# MOCKUP: devuelve 1.0 hasta que exista EbriedadComponet, tambien esta en enano base 
-# TODO: return ebriedad_component.get_modificador_daño()
-func obtener_modificador_ebriedad() -> float:
-	return 1.0
