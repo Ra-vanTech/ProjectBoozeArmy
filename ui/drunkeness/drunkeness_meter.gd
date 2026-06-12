7e617351f6585a7cbadadafef1c48b048e885d7c
MishellPG <pradogordillomichael@gmail.com>
2026-06-11 19:26:55 -0600

    [refactor]Agregar signal de drenkess y comparacion en su cambio con respecto al frame anterior

--- ui/drunkeness/drunkeness_meter.gd
+++ ui/drunkeness/drunkeness_meter.gd
@@ -1,14 +1,28 @@
 class_name DrunkenessMeter
 extends Label
 
+
+#señal para ebriedad 
+signal sobriety_critical_changed(is_critical: bool)
+
 @export var starting_drunkeness: int = 50
 @export var max_drunkeness: int = 100
 
+
 var drunkeness: int:
 	set(input):
+		var previous: int = drunkeness
 		drunkeness = clamp(input, 0, max_drunkeness)
 		text = "Ebriedad: " + str(drunkeness)
 		modulate = Color.RED.lerp(Color.WHITE, float(drunkeness) / max_drunkeness)
+
+#Compara si el estado "critico" cambió respectoel frame anterior.
+		var was_critical: bool = previous == 0
+		var is_critical: bool = drunkeness == 0
+		if was_critical != is_critical:
+			sobriety_critical_changed.emit(is_critical)
+
+
 var timer: Timer
 
 
