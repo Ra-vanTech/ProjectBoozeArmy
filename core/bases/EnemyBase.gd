8a83040280785a53acc37908a8898d307c5a77d3
Yael <243678@ids.upchiapas.edu.mx>
2026-07-01 00:16:28 -0600

    refactor: eliminar señal redundante

--- core/bases/EnemyBase.gd
+++ core/bases/EnemyBase.gd
@@ -1,11 +1,10 @@
 class_name EnemyBase
 extends CharacterBody3D
 
-signal enemy_died(xp_amount: int)
-
+# signal enemy_died(xp_amount: int)
 @export var health: float = 100.0
 @export var COINS_DROPPED: int = 25
 @export var speed_multiplier: float = 1.0
-#valor xp por tipo 
-# esqueleto: 5 | smile: 8 | murcielago: 4 | Invocador 10 
+#valor xp por tipo
+# esqueleto: 5 | smile: 8 | murcielago: 4 | Invocador 10
 @export var xp_value: int = 0
