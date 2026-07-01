5ff55a84eab16ec5b2c514199730247e6c52f9e1
Yael <243678@ids.upchiapas.edu.mx>
2026-07-01 01:51:44 -0600

    refactor: función para crear drops
    sigo sin entender por qué no deja crear instancias de la cerveza, por x
    o y razón no las hace bien y se vuelven nulas

--- entities/enemies/states/enemy_dead.gd
+++ entities/enemies/states/enemy_dead.gd
@@ -4,24 +4,26 @@ extends State
 @export var parent: EnemyBase
 
 var xp_orb: PackedScene = load("res://entities/drops/xp_drop/XpDrop.tscn")
-var beer_drop: PackedScene = load("res://entities/drops/beer_drop/BeerDrop.tscn")
+var beer_pickup: PackedScene = load("res://entities/drops/beer_drop/BeerDrop.tscn")
 
 
 func enter():
-	var new_xp_orb = xp_orb.instantiate() as XpDrop
-	new_xp_orb.global_position = parent.global_position + Vector3(randi_range(-1, 1), 0, randi_range(-1, 1))
-	new_xp_orb.bonus_amount = parent.xp_value
-	parent.add_sibling(new_xp_orb)
-
-	print(beer_drop)
+	create_drop(xp_orb, parent.xp_value)
 
 	var beer_chance = randi_range(0, 3)
 	if beer_chance == 1:
-		print(beer_drop)
-		var beer = beer_drop.instantiate() as BeerDrop
-		print(beer == null)
-		beer.global_position = parent.global_position + Vector3(randi_range(-1, 1), 0, randi_range(-1, 1))
-		beer.bonus_amount = 15
-		parent.add_sibling(beer)
+		create_drop(beer_pickup, 15)
+		# beer.global_position = parent.global_position + Vector3(randi_range(-1, 1), 0, randi_range(-1, 1))
+		# beer.bonus_amount = 15
+		# parent.add_sibling(beer)
 	# parent.enemy_died.emit(parent.xp_value)
 	parent.queue_free()
+
+
+func create_drop(drop: PackedScene, bonus: int) -> void:
+	print(drop)
+	var new_drop = drop.instantiate() as DropBase
+	print(new_drop == null)
+	new_drop.global_position = parent.global_position + Vector3(randi_range(-1, 1), 0, randi_range(-1, 1))
+	new_drop.bonus_amount = bonus
+	parent.add_sibling(new_drop)
