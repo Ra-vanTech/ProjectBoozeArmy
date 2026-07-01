7a7b887330e217c714fb674172e969d359d093bc
Yael <243678@ids.upchiapas.edu.mx>
2026-06-30 21:11:10 -0600

    feat: nodos iniciales para el sistema de drops

--- core/bases/DropBase.gd
+++ core/bases/DropBase.gd
@@ -1,10 +1,6 @@
 class_name DropBase
 extends Node
 
-signal on_pickup
-
-@export var lol: int
-
 
 func drop() -> void:
 	pass
---
+++ entities/drops/xp_drop/xp_drop.gd
@@ -0,0 +1,4 @@
+class_name XpDrop
+extends DropBase
+
+@export var xp_amount: int
