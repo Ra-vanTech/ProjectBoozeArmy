095c13976a0c7ed36de62695a423ccd1229ebd52
Yae <243678@ids.upchiapas.edu.mx>
2026-07-08 15:05:17 -0600

    fix: eliminar máquina de estado del gestor del juego
    no se utilizará

--- core/managers/game_manager/GameManager.gd
+++ core/managers/game_manager/GameManager.gd
@@ -4,7 +4,6 @@ extends Node
 @export var xp_manager: XPManager
 @export var drunkeness_manager: DrunkenessManager
 @export var upgrade_manager: UpgradeManager
-@export var game_state: StateMachine
 @export var difficulty_manager: DifficultyManager
 @export var money_manager: MoneyManager
 
