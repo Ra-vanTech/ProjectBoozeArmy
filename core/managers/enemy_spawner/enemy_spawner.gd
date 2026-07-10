59f386952943e0a7e2e0dfc77bfc34eb899f2b6c
Yae <243678@ids.upchiapas.edu.mx>
2026-07-10 11:48:16 -0600

    feat: rangos mínimos y máximos para la aparición de enemigos

--- core/managers/enemy_spawner/enemy_spawner.gd
+++ core/managers/enemy_spawner/enemy_spawner.gd
@@ -3,6 +3,8 @@ extends Node3D
 
 @export var enemy: PackedScene
 @export var spawn_delay: float = 10.0 # Después de cuánto tiempo va a aparecer el enemigo
+@export_range(1, 19) var MIN_SPAWN_DISTANCE = 5
+@export_range(19, 50) var MAX_SPAWN_DISTANCE = 20
 
 var timer: Timer
 var spawn_cooldown: float = 3.0
@@ -38,10 +40,19 @@ func spawn_enemy() -> void:
 		new_enemy.health *= game_manager.upgrade_manager.get_enemy_hp()
 
 		add_child(new_enemy)
-		new_enemy.global_position = global_position + Vector3(randi_range(-20, 20), 2, randi_range(-20, 20))
+		new_enemy.global_position = global_position + Vector3(get_spawn_range(randi_range(-20, 20)), 1, get_spawn_range(randi_range(-20, 20)))
 
 		# Conectar XP al morir, la señal pasa xp_value directamente a add_xp
 		# new_enemy.enemy_died.connect(game_manager.add_xp)
 
 	timer.wait_time = game_manager.difficulty_manager.get_spawn_rate()
 	spawn_amount = game_manager.difficulty_manager.get_spawn_amount()
+
+
+# Evita que los enemigos aparezcan fuera del rango especificado por MIN_SPAWN_DISTANCE y MAX_SPAWN_DISTANCE
+func get_spawn_range(pos: int) -> int:
+	if pos < 0:
+		return clamp(pos, -MAX_SPAWN_DISTANCE, -MIN_SPAWN_DISTANCE)
+	# si se optiene un 0, el enemigo aparecería en el centro
+	# con clamp, se fuerza a aparecer a la distancia mínima
+	return clamp(pos, MIN_SPAWN_DISTANCE, MAX_SPAWN_DISTANCE)
