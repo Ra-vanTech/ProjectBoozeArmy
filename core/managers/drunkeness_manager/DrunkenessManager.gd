7cd878cf99248c282b9828ba1852103a97632e91
Yae <243678@ids.upchiapas.edu.mx>
2026-07-10 12:40:06 -0600

    feat: crecimiento adicional con niveles de ebriedad más altos

--- core/managers/drunkeness_manager/DrunkenessManager.gd
+++ core/managers/drunkeness_manager/DrunkenessManager.gd
@@ -43,8 +43,12 @@ func calculate_damage_multiplier() -> float:
 		return 0.7
 	elif drunkeness <= 70:
 		return 1.0
-	else:
+	elif drunkeness <= 100:
 		return 1.3
+	else:
+		# permite crecimiento de daño adicional
+		# no es un bono tan grande, según mi calculadora una ebriedad de 1000 solo daría un bono adicional de 0.54
+		return 1.3 + pow(drunkeness - 100, 0.25) / 10.0
 
 
 func _tick() -> void:
