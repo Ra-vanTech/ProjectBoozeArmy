ec269944c80ee40e480ffc5fc6b3cf84d468bec8
Yae <243678@ids.upchiapas.edu.mx>
2026-07-14 21:51:46 -0600

    feat: cambio de MAX_LEVEL de variable exportada a que tome el valor persistente

--- core/persistence/persistence_manager.gd
+++ core/persistence/persistence_manager.gd
@@ -1,29 +1,36 @@
 extends Node
 
-const DATA_PATH: String = "user://save_test.json"
+const DATA_PATH: String = "user://data.json"
 
 var data: Dictionary = {
-	"test_value": 67,
+	"gold": 0,
+	"max_level": 10,
 }
 
 
 func _ready() -> void:
 	load_data()
-	data["lol"] = true
-	save_data()
 
 
 func save_data() -> void:
+	print(data, " (opening)")
+	# Lo considero bueno por que si no se cargan los datos al cargar los que estaban en
+	# el archivo y no en data o tenian valores distintos se eliminan/sobreescriben
+	# NOTA: los sigue sobreescribiendo, alch no entiendo
+	load_data()
 	var file: FileAccess = FileAccess.open(DATA_PATH, FileAccess.WRITE)
 	file.store_var(data)
 	file.close()
+	print(data, " (closure)")
 
 
 func load_data() -> void:
 	if FileAccess.file_exists(DATA_PATH):
 		var file: FileAccess = FileAccess.open(DATA_PATH, FileAccess.READ)
 		var extracted_data: Dictionary = file.get_var()
+		print(extracted_data, " (extracted)")
 		for i in extracted_data: # Así no borra datos que aún no se hayan guardado al hacer la carga de datos
 			if data.has(i):
 				data[i] = extracted_data[i]
+				print(data, " (loop)")
 		file.close()
