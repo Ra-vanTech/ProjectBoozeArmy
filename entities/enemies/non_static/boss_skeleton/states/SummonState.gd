0b316ac78424c80a806a7c3709a1403e71462154
MishellPG <pradogordillomichael@gmail.com>
2026-07-16 21:59:27 -0600

    [feat] se agrego un time para la invocacion haciendo una pausa en cuanto invoca para despues moverse, el mismo ritmo que el ataque

--- entities/enemies/non_static/boss_skeleton/states/SummonState.gd
+++ entities/enemies/non_static/boss_skeleton/states/SummonState.gd
@@ -3,38 +3,51 @@ extends State
 
 @export var summoned_enemy: PackedScene
 @export var amount: int = 2
+@export var summon_pause: float = 1.0
 @export var spawn_radius: float = 3.5
 
 @export var owner_body: Node3D
 
+var timer: Timer
+
+
+func _ready() -> void:
+	timer = Timer.new()
+	timer.one_shot = true
+	add_child(timer)
+	timer.timeout.connect(_decide_next_state)
 
 func enter() -> void:
-    _summon()
-    _decide_next_state()
+	_summon()
+	timer.start(summon_pause)
+
+func exit() -> void:
+	timer.stop()
+
 
 func _summon() -> void:
-    if not summoned_enemy or not is_instance_valid(owner_body):
-        return
-    var parent := owner_body.get_parent()
-    if not is_instance_valid(parent):
-        return
-    
-    for i in range(amount):
-        var new_enemy := summoned_enemy.instantiate() as Node3D
-        parent.add_child(new_enemy)
-        new_enemy.global_position = _random_position(owner_body.global_position)
+	if not summoned_enemy or not is_instance_valid(owner_body):
+		return
+	var parent := owner_body.get_parent()
+	if not is_instance_valid(parent):
+		return
+	
+	for i in range(amount):
+		var new_enemy := summoned_enemy.instantiate() as Node3D
+		parent.add_child(new_enemy)
+		new_enemy.global_position = _random_position(owner_body.global_position)
 
 func _random_position(origin: Vector3) -> Vector3:
-    var angle := randf() * TAU
-    var distance := randf_range(1.0, spawn_radius)
-    return origin + Vector3(cos(angle) * distance, 0.0, sin(angle) * distance)
+	var angle := randf() * TAU
+	var distance := randf_range(1.0, spawn_radius)
+	return origin + Vector3(cos(angle) * distance, 0.0, sin(angle) * distance)
 
 
 func _decide_next_state() -> void:
-    if is_instance_valid(owner_body.player_in_range):
-        state_machine.change_state("EnemyAttackState")
-    else:
-        state_machine.change_state("EnemyMovingState")
+	if is_instance_valid(owner_body.player_in_range):
+		state_machine.change_state("EnemyAttackState")
+	else:
+		state_machine.change_state("EnemyMovingState")
 
 
 #Se trata de un invocador que no toma en cuenta el terreno 
