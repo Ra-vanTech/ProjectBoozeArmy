880e01ab65e8daf4bb39a8f68caa8c5275ae781a
Yae <243678@ids.upchiapas.edu.mx>
2026-07-17 12:31:54 -0600

    feat: avances en la interfaz de mejoras
    aun va por ahi pero ya va mejor

--- ui/upgrade_store/upgrade_store.gd
+++ ui/upgrade_store/upgrade_store.gd
@@ -1,5 +1,15 @@
 extends CanvasLayer
 
+const max_levels: Dictionary = {
+	Store.DATA.MAX_LVL: 0,
+	Store.DATA.STARTING_DWARVES: 10,
+	Store.DATA.DMG_MAX_LVL: 0,
+	Store.DATA.ATK_SPEED_MAX_LVL: 100,
+	Store.DATA.DWARF_LIMIT_MAX_LVL: 0,
+	Store.DATA.DRUNKENNESS_MAX_LVL: 10,
+	Store.DATA.ENEMY_HP_REDUCTION_MAX_LVL: 20,
+}
+
 @onready var player_anchor: HBoxContainer = $ColorRect/CenterContainer/VBoxContainer/Panel/Jugador/Container
 @onready var upgrade_anchor: HBoxContainer = $ColorRect/CenterContainer/VBoxContainer/Panel/Niveles/Container
 @onready var gold_container: Label = $ColorRect/CenterContainer/VBoxContainer/Gold
@@ -8,17 +18,19 @@ extends CanvasLayer
 func _ready() -> void:
 	$TransitionScreen/AnimationPlayer.play("fade_out")
 	gold_container.text = "Oro: " + str(Store.save[Store.DATA.GOLD])
-	create_card(player_anchor, "Nivel máximo", "Incrementa el nivel máximo del jugador", 500, 1)
+	create_card(player_anchor, "Nivel máximo", "Incrementa el nivel máximo del jugador", 500, 1.1)
 	create_card(player_anchor, "Enanos iniciales", "Incrementa los enanos que se tienen al iniciar la partida", 2000, 1.5)
-	for thingy in UpgradeManager.UpgradeType:
-		print(thingy)
-	pass
+	# for thingy in UpgradeManager.UpgradeType:
+	# 	print(thingy)
+	# pass
 
 
 # es como hacer html desde js puro, me cae mal
 # se me hace más fácil que hacer cada cuadro individualmente
 func create_card(anchor: HBoxContainer, _title: String, _description: String, _cost: int, cost_increase: float) -> void:
-	var cost_stored: int = _cost
+	var cost_stored: StoredPrice = StoredPrice.new()
+	cost_stored.cost = _cost
+
 	var bg: PanelContainer = PanelContainer.new()
 	bg.custom_minimum_size = Vector2(200, -1)
 
@@ -39,12 +51,18 @@ func create_card(anchor: HBoxContainer, _title: String, _description: String, _c
 
 	var cost: Button = Button.new()
 	# Por alguna razón el precio solo se actualiza una vez
-	var test = func something() -> void:
-		cost_stored *= cost_increase
-		cost.text = "Comprar: " + str(cost_stored)
-		print(cost_stored)
-	cost.text = "Comprar: " + str(cost_stored)
-	cost.pressed.connect(test)
+
+	var check_affordability = func() -> void:
+		if Store.save[Store.DATA.GOLD] < cost_stored.cost:
+			cost.disabled = true
+
+	var purchase = func() -> void:
+		cost_stored.cost *= cost_increase
+		cost.text = "Comprar: " + _format_price(cost_stored.cost)
+		check_affordability.call()
+
+	cost.text = "Comprar: " + _format_price(cost_stored.cost)
+	cost.pressed.connect(purchase)
 
 	container.add_child(title)
 	container.add_child(description)
@@ -57,8 +75,11 @@ func create_card(anchor: HBoxContainer, _title: String, _description: String, _c
 	pass
 
 
-func aaa() -> void:
-	print("aa")
+func _format_price(price: int) -> String:
+	const preffix: String = "$"
+	if price > 1000:
+		return preffix + str(price / 1000 + snapped((price % 1000) / 1000.0, 0.01)) + "k"
+	return preffix + str(price)
 
 
 func _on_timer_timeout() -> void:
@@ -69,3 +90,10 @@ func _on_button_pressed() -> void:
 	$TransitionScreen.show()
 	$TransitionScreen/AnimationPlayer.play("fade_in")
 	$TransitionScreen/Timer.start()
+
+
+# Por alguna razón, las funciones lambda en gdscript no pueden modificar variables externas a pesar
+# de que sí pueda acceder a ellas, así que se le tienen que dar valores que se pasan por referencia
+# (arreglos, diccionarios, objetos, clases, etc...)
+class StoredPrice:
+	var cost: int
