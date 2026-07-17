a6aa13859ad3457c7c849719c804d5b1a74c6a0e
Yae <243678@ids.upchiapas.edu.mx>
2026-07-17 13:31:18 -0600

    feat: adición de mejoras de nivel máximo
    No me parece que vayan a entrar dx

--- ui/upgrade_store/upgrade_store.gd
+++ ui/upgrade_store/upgrade_store.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+signal purchase_done
+
 const max_levels: Dictionary = {
 	Store.DATA.MAX_LVL: 0,
 	Store.DATA.STARTING_DWARVES: 10,
@@ -19,7 +21,11 @@ func _ready() -> void:
 	$TransitionScreen/AnimationPlayer.play("fade_out")
 	gold_container.text = "Oro: " + str(Store.save[Store.DATA.GOLD])
 	create_card(player_anchor, Store.DATA.MAX_LVL, "Nivel máximo", "Incrementa el nivel máximo del jugador", 500, 1.1)
-	create_card(player_anchor, Store.DATA.STARTING_DWARVES, "Enanos iniciales", "Incrementa los enanos que se tienen al iniciar la partida", 2000, 1.5)
+	create_card(player_anchor, Store.DATA.STARTING_DWARVES, "Enanos iniciales", "Más enanos vienen a ayudarte", 2000, 1.5)
+
+	create_card(upgrade_anchor, Store.DATA.DMG_MAX_LVL, "Daño máximo", "Los enanos atacan con más fuerza", 1000, 1.2)
+	create_card(upgrade_anchor, Store.DATA.ATK_SPEED_MAX_LVL, "Velocidad de ataque", "Los enanos atacan más rápido", 1000, 1.2)
+	create_card(upgrade_anchor, Store.DATA.DWARF_LIMIT_MAX_LVL, "Enanos de apoyo", "Más enanos están dispuesto a unirse a ti", 2000, 1.3)
 	# for thingy in UpgradeManager.UpgradeType:
 	# 	print(thingy)
 	# pass
@@ -61,6 +67,7 @@ func create_card(anchor: HBoxContainer, store_idx: int, _title: String, _descrip
 		if Store.save[store_idx] >= max_levels[store_idx] and max_levels[store_idx] != 0:
 			cost.disabled = true
 			cost.text = "MAX"
+	purchase_done.connect(check_availability)
 
 	var purchase = func() -> void:
 		Store.save[Store.DATA.GOLD] -= cost_stored.cost
@@ -70,7 +77,8 @@ func create_card(anchor: HBoxContainer, store_idx: int, _title: String, _descrip
 		cost_stored.cost *= cost_increase
 		cost.text = "Comprar: " + _format_price(cost_stored.cost)
 		level_display.text = "Nivel actual: " + str(Store.save[store_idx])
-		check_availability.call()
+		purchase_done.emit()
+		# check_availability.call()
 
 	cost.text = "Comprar: " + _format_price(cost_stored.cost)
 	check_availability.call()
@@ -90,7 +98,7 @@ func create_card(anchor: HBoxContainer, store_idx: int, _title: String, _descrip
 
 func _format_price(price: int) -> String:
 	const preffix: String = "$"
-	if price > 1000:
+	if price >= 1000:
 		return preffix + str(price / 1000 + snapped((price % 1000) / 1000.0, 0.01)) + "k"
 	return preffix + str(price)
 
